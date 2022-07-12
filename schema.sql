9b1d95b820da4b6d205a1c16737eda5610081c55
AlphaNtihinduka <ntihindukaalpha@yahoo.com>
2022-07-13 00:06:49 +0300

    create,insert ,and  select values into vet_clinic

--- schema.sql
+++ schema.sql
@@ -1,5 +1,9 @@
 /* Database schema to keep the structure of entire database. */
 
-CREATE TABLE animals (
-    name varchar(100)
-);
+CREATE TABLE animals(
+id int PRIMARY KEY NOT NULL,
+name VARCHAR(30) NOT NULL,
+date_of_birth DATE,
+escape_attempts INT,
+neutered BOOL,
+weight_kg DECIMAL);
